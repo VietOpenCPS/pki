91be6ee26f7c58fb6cf75bba99b8ed1d27669d6a
nguyennv <nguyennv1981@gmail.com>
2016-06-16 18:08:53 +0700

    Update fdf signer test

--- server/src/test/java/org/opencps/pki/PdfSignerTest.java
+++ server/src/test/java/org/opencps/pki/PdfSignerTest.java
@@ -16,6 +16,20 @@
 */
 package org.opencps.pki;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
@@ -24,13 +38,19 @@ import junit.framework.TestSuite;
  * @author Nguyen Van Nguyen <nguyennv@iwayvietnam.com>
  */
 public class PdfSignerTest extends TestCase {
+    private static final String certPath = "./src/test/java/resources/cert.pem";
+    private static final String pdfPath = "./src/test/java/resources/opencps.pdf";
 
-	/**
-	 * Create the test case
-	 */
-	public PdfSignerTest(String testName) {
-		// TODO Auto-generated constructor stub
-	}
+    X509Certificate cert;
+    CertificateFactory cf;
+    PdfSigner signer;
+
+    /**
+     * Create the test case
+     */
+    public PdfSignerTest(String testName) {
+        super(testName);
+    }
 
     /**
      * @return the suite of tests being tested
@@ -40,4 +60,15 @@ public class PdfSignerTest extends TestCase {
         return new TestSuite(PdfSignerTest.class);
     }
 
+    protected void setUp() throws CertificateException, FileNotFoundException {
+        cf = CertificateFactory.getInstance("X.509");
+        cert = (X509Certificate) cf.generateCertificate(new FileInputStream(new File(certPath)));
+        signer = new PdfSigner(pdfPath, cert);
+    }
+    
+    public void testFilePath() {
+    	assertEquals(pdfPath, signer.getOriginFilePath());
+    	assertEquals("./src/test/java/resources/opencps.temp.pdf", signer.getTempFilePath());
+    	assertEquals("./src/test/java/resources/opencps.signed.pdf", signer.getSignedFilePath());
+    }
 }
