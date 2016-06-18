6e48a8825ceb71b12dd4b64c7a3cc69291582219
nguyennv <nguyennv1981@gmail.com>
2016-06-18 10:22:07 +0700

    First commit

---
+++ client/opencps-pki.js
@@ -0,0 +1,10 @@
+/*!
+ * OpenCPS PKI; version: 0.0.1
+ * https://github.com/VietOpenCPS/pki
+ * Copyright (c) 2016 OpenCPS Community;
+ * Licensed under the AGPL V3+.
+ * https://www.gnu.org/licenses/agpl-3.0.html
+ */
+(function($) {
+
+})(jQuery);
