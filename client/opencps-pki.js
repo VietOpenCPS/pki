4f3554b6259de52041f5b8ee96f41ddd6aae903a
nguyennv <nguyennv1981@gmail.com>
2016-06-22 13:49:24 +0700

    Update client

--- client/opencps-pki.js
+++ client/opencps-pki.js
@@ -6,5 +6,37 @@
  * https://www.gnu.org/licenses/agpl-3.0.html
  */
 (function($) {
+"use strict";
+
+if (window.hwcrypto) {
+    window.hwcrypto.use('auto');
+    window.hwcrypto.debug().then(function(response) {
+      console.log('Debug: ' + response);
+    }, function(err) {
+      console.log('debug() failed: ' + err);
+      return;
+    });
+}
+
+$.extend({
+    getCertificate: function(){
+        var cert = null;
+        if (window.hwcrypto) {
+            window.hwcrypto.getCertificate({lang: 'en'}).then(function(response) {
+                cert = hexToPem(response.hex);
+            }, function(err) {
+              console.log("getCertificate() failed: " + err);
+            });
+        }
+        return cert;
+    },
+    sign: function(options) {
+      window.hwcrypto.sign(cert, {type: 'SHA-256', hex: hash}, {lang: 'en'}).then(function(response) {
+        console.log("Generated signature:\n" + response.hex.match(/.{1,64}/g).join("\n"));
+      }, function(err) {
+        console.log("sign() failed: " + err);
+      });
+    }
+});
 
 })(jQuery);
