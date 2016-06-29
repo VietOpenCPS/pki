c500a46305710f73f6d144fe9e31a82845a0eab0
nguyennv <nguyennv1981@gmail.com>
2016-06-29 09:42:12 +0700

    First commit

---
+++ server/src/main/java/org/opencps/pki/XMLSigner.java
@@ -0,0 +1,51 @@
+/**
+* OpenCPS PKI is the open source PKI Integration software
+* Copyright (C) 2016-present OpenCPS community
+
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* any later version.
+
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU Affero General Public License for more details.
+* You should have received a copy of the GNU Affero General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>
+*/
+package org.opencps.pki;
+
+/**
+ * Signer for xml document
+ * @author Nguyen Van Nguyen <nguyennv@iwayvietnam.com>
+ */
+public class XMLSigner extends BaseSigner {
+
+    /**
+     * Constructor
+     * @throws InvalidAlgorithmParameterException 
+     * @throws NoSuchAlgorithmException 
+     */
+    public XMLSigner() {
+    }
+
+    @Override
+    public byte[] computeHash() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Boolean sign(byte[] signature) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Boolean sign(byte[] signature, String filePath) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
