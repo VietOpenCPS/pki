88a1bbc30bae8535f2007035874803da56cf12ef
nguyennv <nguyennv1981@gmail.com>
2016-07-14 11:27:22 +0700

    Add xml unit test cases

---
+++ server/src/test/java/org/opencps/pki/XmlVerifierTest.java
@@ -0,0 +1,44 @@
+/**
+* OpenCPS PKI is the open source PKI Integration software
+* Copyright (C) 2016-present OpenCPS community
+
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* any later version.
+
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU Affero General Public License for more details.
+* You should have received a copy of the GNU Affero General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>
+*/
+package org.opencps.pki;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * @author Nguyen Van Nguyen <nguyennv@iwayvietnam.com>
+ */
+public class XmlVerifierTest extends TestCase {
+    /**
+     * Create the test case
+     */
+    public XmlVerifierTest(String testName) {
+        super(testName);
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite() {
+        return new TestSuite(XmlVerifierTest.class);
+    }
+    
+    public void testXmlVerifier() {
+    }
+
+}
