90b903fcf30641695f2010d367191d5cf2153b1e
nguyennv <nguyennv1981@gmail.com>
2016-07-14 11:32:07 +0700

    Remove duplicate method

--- server/src/main/java/org/opencps/pki/PdfPkcs7Signer.java
+++ server/src/main/java/org/opencps/pki/PdfPkcs7Signer.java
@@ -27,43 +27,11 @@ public class PdfPkcs7Signer extends PdfSigner {
     }
 
     /**
-     * (non-Javadoc)
-     * @throws SignatureException 
-     * @see org.opencps.pki.Signer#computeHash()
-     */
-    @Override
-    public byte[] computeHash() throws SignatureException {
-        float llx = 36.0f;
-        float lly = 48.0f;
-        SignatureImage signatureImage = getSignatureGraphic();
-        if (signatureImage != null) {
-            int signatureImageWidth = signatureImage.getBufferedImage().getWidth();
-            int signatureImageHeight = signatureImage.getBufferedImage().getHeight();
-            float urx = llx + signatureImageWidth;
-            float ury = lly + signatureImageHeight;
-            return computeDigest(llx, lly, urx, ury);
-        }
-        else {
-            return computeDigest(llx, lly, 144, 80);
-        }
-    }
-
-    /**
      * Compute hash key with corner coordinates of rectangle
      */
     @Override
     public byte[] computeHash(float llx, float lly, float urx, float ury) throws SignatureException {
         return computeDigest(llx, lly, urx, ury);
-    }
-
-    /**
-     * (non-Javadoc)
-     * @throws SignatureException 
-     * @see org.opencps.pki.Signer#sign()
-     */
-    @Override
-    public Boolean sign(byte[] signature) throws SignatureException {
-        return sign(signature, getTempFilePath());
     }
 
     /**
