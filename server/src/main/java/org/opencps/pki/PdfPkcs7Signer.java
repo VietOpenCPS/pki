b79a451336fbdd3f19921601191e8e824bd8df60
nguyennv <nguyennv1981@gmail.com>
2016-07-21 16:56:40 +0700

    Add license header and class description

--- server/src/main/java/org/opencps/pki/PdfPkcs7Signer.java
+++ server/src/main/java/org/opencps/pki/PdfPkcs7Signer.java
@@ -1,14 +1,28 @@
 /**
- * 
- */
+* OpenCPS PKI is the open source PKI Integration software
+* Copyright (C) 2016-present OpenCPS community
+
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* any later version.
+
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU Affero General Public License for more details.
+* You should have received a copy of the GNU Affero General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>
+*/
 package org.opencps.pki;
 
 import java.security.SignatureException;
 import java.security.cert.X509Certificate;
 
 /**
- * @author nguyennv
+ * Signer for pdf document using encoded pkcs7
  *
+ * @author Nguyen Van Nguyen <nguyennv@iwayvietnam.com>
  */
 public class PdfPkcs7Signer extends PdfSigner {
 
